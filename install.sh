ce2bba3170e7b8c00a71a223a12bcc6f50baa264
Frank Harkins <frankharkins@hotmail.co.uk>
2023-04-21 16:54:17 +0100

    Update packages and Python version

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ set -e
 
 # Setup Python venv and install packages
 echo "Setting up Python virtual environment in ./.venv"
-python3.8 -m venv ./.venv
+python3.10 -m venv ./.venv
 . .venv/bin/activate
-python3.8 -m pip install -r environment/requirements.txt
+python3.10 -m pip install -r environment/requirements.txt
 
 # Create default profile, then copy over our custom settings
 export IPYTHONDIR="./environment/ipython"
